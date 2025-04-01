26d45beda9d37f3433cc201f98e0ad38a55dad99
Sean Sullivan <112829052+RVCA212@users.noreply.github.com>
2025-04-01 03:18:08 +0000

    Add nodes/second_node.py

---
+++ nodes/second_node.py
@@ -0,0 +1,21 @@
+"""
+Node implementation for second_node
+"""
+
+from typing import Dict, Any, TypedDict
+
+# Import your state from the state file
+from state import State
+
+def second_node(state: State) -> State:
+    """
+    Implementation for second_node
+
+    Args:
+        state: The current state of the graph
+
+    Returns:
+        Updated state
+    """
+    # TODO: Implement second_node logic
+    return state
